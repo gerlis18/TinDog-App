d530f18e6d3ab0d83c6d84c6c094489fd0cfdcc7
simpleap <gerlis1997@gmail.com>
2018-05-13 20:20:03 -0500

    update: added images (nope & like)

--- Tindog/Controllers/HomeViewController.swift
+++ Tindog/Controllers/HomeViewController.swift
@@ -18,6 +18,8 @@ class HomeViewController: UIViewController {
 
     @IBOutlet weak var cardView: UIView!
     @IBOutlet weak var homeWrapper: UIStackView!
+    @IBOutlet weak var likeImage: UIImageView!
+    @IBOutlet weak var nopeImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,14 +34,34 @@ class HomeViewController: UIViewController {
     @objc func cardDragged(gestureRecognized : UIPanGestureRecognizer) {
         let cardPoint = gestureRecognized.translation(in: view)
         self.cardView.center = CGPoint(x: self.view.bounds.width / 2 + cardPoint.x, y: self.view.bounds.height / 2 + cardPoint.y)
+        
+        let xFromCenter = (self.view.bounds.width / 2) - self.cardView.center.x
+        var rotate = CGAffineTransform(rotationAngle: xFromCenter / 500)
+        let scale = min(100 / abs(xFromCenter), 1)
+        var finalTransform = rotate.scaledBy(x: scale, y: scale)
+        self.cardView.transform = finalTransform
+        
+        if (self.cardView.center.x < (self.view.bounds.width / 2 - 100)) {
+            self.nopeImage.alpha = min(abs(xFromCenter) / 100, 1)
+        }
+        
+        if (self.cardView.center.x > (self.view.bounds.width / 2 + 100)) {
+            self.likeImage.alpha = min(abs(xFromCenter) / 100, 1)
+        }
+        
+        
         if (gestureRecognized.state == .ended) {
             if (self.cardView.center.x < (self.view.bounds.width / 2 - 100)) {
-                
+                self.nopeImage.alpha = 0
             }
             
             if (self.cardView.center.x > (self.view.bounds.width / 2 + 100)) {
-                
+                self.likeImage.alpha = 0
             }
+            
+            rotate = CGAffineTransform(rotationAngle: 0)
+            finalTransform = rotate.scaledBy(x: 1, y: 1)
+            self.cardView.transform = finalTransform
             
             self.cardView.center = CGPoint(x: self.homeWrapper.bounds.width / 2, y: (self.homeWrapper.bounds.height / 2 - 35))
         }
